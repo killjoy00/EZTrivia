d92cb607de8259ff0de7e89fec6346673081e74f
killjoy00 <killjoy00@yahoo.com>
2026-09-13 20:59:58 -0500

    Sync local progress to Play Games while authenticated

--- android/app/src/main/java/com/rsm/eztrivia/MainActivity.kt
+++ android/app/src/main/java/com/rsm/eztrivia/MainActivity.kt
@@ -4,18 +4,41 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.MaterialTheme
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import com.rsm.eztrivia.data.PlayerStateStore
 import com.rsm.eztrivia.playgames.PlayGamesManager
 import com.rsm.eztrivia.ui.EZTriviaApp
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
-    private lateinit var playGamesManager: PlayGamesManager
+    lateinit var playGamesManager: PlayGamesManager
+        private set
+
+    private lateinit var playerStateStore: PlayerStateStore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         playGamesManager = PlayGamesManager(this)
+        playerStateStore = PlayerStateStore(applicationContext)
+
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                combine(playerStateStore.state, playGamesManager.connection) { state, connection ->
+                    state to connection
+                }.collect { (state, connection) ->
+                    if (connection.isAuthenticated) {
+                        playGamesManager.sync(state)
+                    }
+                }
+            }
+        }
+
         setContent {
             MaterialTheme {
-                EZTriviaApp(playGamesManager = playGamesManager)
+                EZTriviaApp()
             }
         }
     }
