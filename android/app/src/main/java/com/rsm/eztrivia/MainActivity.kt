6bc8a899138bd564ac7f2eb15a5291bcd4bae75e
killjoy00 <killjoy00@yahoo.com>
2026-09-13 20:58:46 -0500

    Connect MainActivity to Play Games v2

--- android/app/src/main/java/com/rsm/eztrivia/MainActivity.kt
+++ android/app/src/main/java/com/rsm/eztrivia/MainActivity.kt
@@ -4,15 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.MaterialTheme
+import com.rsm.eztrivia.playgames.PlayGamesManager
 import com.rsm.eztrivia.ui.EZTriviaApp
 
 class MainActivity : ComponentActivity() {
+    private lateinit var playGamesManager: PlayGamesManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        playGamesManager = PlayGamesManager(this)
         setContent {
             MaterialTheme {
-                EZTriviaApp()
+                EZTriviaApp(playGamesManager = playGamesManager)
             }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (::playGamesManager.isInitialized) {
+            playGamesManager.refreshAuthentication()
         }
     }
 }
