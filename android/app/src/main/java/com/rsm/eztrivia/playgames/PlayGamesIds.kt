d2082b4cd91d80e122a0ac2770b6c65f0d6ec23e
killjoy00 <killjoy00@yahoo.com>
2026-09-13 20:56:50 -0500

    Add Play Games resource IDs

---
+++ android/app/src/main/java/com/rsm/eztrivia/playgames/PlayGamesIds.kt
@@ -0,0 +1,49 @@
+package com.rsm.eztrivia.playgames
+
+import com.rsm.eztrivia.model.TriviaCategory
+
+/** Google-generated resource IDs for Play Games Services project 406580555223. */
+object PlayGamesIds {
+    val achievements: Map<String, String> = mapOf(
+        "EZTrivia.achievement.first_round" to "CgkI19vb0OoLEAIQAQ",
+        "EZTrivia.achievement.perfect_easy" to "CgkI19vb0OoLEAIQAg",
+        "EZTrivia.achievement.perfect_medium" to "CgkI19vb0OoLEAIQAw",
+        "EZTrivia.achievement.perfect_hard" to "CgkI19vb0OoLEAIQBA",
+        "EZTrivia.achievement.all_categories" to "CgkI19vb0OoLEAIQBQ",
+        "EZTrivia.achievement.all_categories_14" to "CgkI19vb0OoLEAIQBg",
+        "EZTrivia.achievement.streak_7" to "CgkI19vb0OoLEAIQBw",
+        "EZTrivia.achievement.streak_30" to "CgkI19vb0OoLEAIQCA",
+        "EZTrivia.achievement.rounds_10" to "CgkI19vb0OoLEAIQCQ",
+        "EZTrivia.achievement.rounds_50" to "CgkI19vb0OoLEAIQCg",
+        "EZTrivia.achievement.rounds_100" to "CgkI19vb0OoLEAIQCw",
+        "EZTrivia.achievement.points_10000" to "CgkI19vb0OoLEAIQDA",
+        "EZTrivia.achievement.points_50000" to "CgkI19vb0OoLEAIQDQ",
+        "EZTrivia.local.all_categories_16" to "CgkI19vb0OoLEAIQDg",
+        "EZTrivia.local.quick_play_1" to "CgkI19vb0OoLEAIQDw",
+        "EZTrivia.local.quick_play_10" to "CgkI19vb0OoLEAIQEA",
+        "EZTrivia.local.friend_challenges_5" to "CgkI19vb0OoLEAIQEQ",
+        "EZTrivia.local.daily_perfect" to "CgkI19vb0OoLEAIQEg",
+        "EZTrivia.local.streak_100" to "CgkI19vb0OoLEAIQEw",
+    )
+
+    const val dailyLeaderboard = "CgkI19vb0OoLEAIQFA"
+
+    val categoryLeaderboards: Map<TriviaCategory, String> = mapOf(
+        TriviaCategory.FOOTBALL to "CgkI19vb0OoLEAIQFQ",
+        TriviaCategory.BASKETBALL to "CgkI19vb0OoLEAIQFg",
+        TriviaCategory.SOCCER to "CgkI19vb0OoLEAIQFw",
+        TriviaCategory.FLAGS to "CgkI19vb0OoLEAIQGA",
+        TriviaCategory.HISTORY to "CgkI19vb0OoLEAIQGQ",
+        TriviaCategory.SCIENCE to "CgkI19vb0OoLEAIQGg",
+        TriviaCategory.MOVIES to "CgkI19vb0OoLEAIQGw",
+        TriviaCategory.TV to "CgkI19vb0OoLEAIQHA",
+        TriviaCategory.GEOGRAPHY to "CgkI19vb0OoLEAIQHQ",
+        TriviaCategory.MUSIC to "CgkI19vb0OoLEAIQHg",
+        TriviaCategory.ANIMALS to "CgkI19vb0OoLEAIQHw",
+        TriviaCategory.FOOD to "CgkI19vb0OoLEAIQIA",
+        TriviaCategory.LITERATURE to "CgkI19vb0OoLEAIQIQ",
+        TriviaCategory.ART to "CgkI19vb0OoLEAIQIg",
+        TriviaCategory.MYTHOLOGY to "CgkI19vb0OoLEAIQIw",
+        TriviaCategory.VIDEO_GAMES to "CgkI19vb0OoLEAIQJA",
+    )
+}
