bcb47a5a953debcb882756d45829f360ad439784
killjoy00 <killjoy00@yahoo.com>
2026-09-13 23:45:32 -0500

    Fix Compose weight scope import

--- android/app/src/main/java/com/rsm/eztrivia/MainActivity.kt
+++ android/app/src/main/java/com/rsm/eztrivia/MainActivity.kt
@@ -6,7 +6,6 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.weight
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
